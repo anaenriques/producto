58699135d3d7fdaf87a482a62e42a1f7024ce9e6
vsanchcu <victoriano.sanchezcuenca@everis.nttdata.com>
2021-06-10 16:04:59 +0200

    Corrección estructura 2

--- producto/src/main/java/com/producto/controller/ProductController.java
+++
@@ -1,8 +0,0 @@
-package com.producto.controller;
-
-import org.springframework.web.bind.annotation.RestController;
-
-@RestController
-public class ProductController {
-
-}
--- producto/src/main/resources/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO PRODUCT (name,description, product_code, type, family) VALUES
-  ('Pera','Alimento para merendar', '1254JKK', 'fruit', 'fruit'),
-  ('Manzana','Alimento para comer', '0025LL', 'fruit', 'fruit'),
-  ('Fresa','Alimento para cenar', '2343232GHG', 'fruit', 'fruit');
