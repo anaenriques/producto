ab0e933879d2bb0c10d31214000426e714998663
Germán ZM <gzuritam@everis.com>
2021-06-11 13:37:33 +0200

    Conf DataSource

--- src/main/java/com/producto/configuration/AppConfiguration.java
+++ src/main/java/com/producto/configuration/AppConfiguration.java
@@ -2,11 +2,7 @@ package com.producto.configuration;
 
 import java.io.IOException;
 import java.net.Socket;
-import java.net.UnknownHostException;
-
 import javax.sql.DataSource;
-
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -15,34 +11,37 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class AppConfiguration {
 	
-	@Autowired
-	DataSource dataSource;
-	
-	@Bean(initMethod = "start", destroyMethod = "stop")
 	@Primary
-	public void startConnection() {
+	@Bean()
+	public DataSource startConnection() {
+		
+		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
+		
+        dataSourceBuilder.driverClassName("org.h2.Driver");
+        dataSourceBuilder.username("sa");
+        dataSourceBuilder.password("");
+		
+		if(isPortInUse("localhost", 9090))
+	        dataSourceBuilder.url("jdbc:h2:tcp://localhost:9090/mem:sinsajodb");
+		else
+	        dataSourceBuilder.url("jdbc:h2:mem:productodb");
+		
+		return dataSourceBuilder.build();
+		
+	}
+	
+	private boolean isPortInUse(String hostName, int portNumber) {
+		boolean result = false;
+		
 		try {
 			Socket socket = new Socket("localhost", 9090);
-			DataSourceBuilder<DataSource> dataSourceBuilder = (DataSourceBuilder<DataSource>) DataSourceBuilder.create();
-	        dataSourceBuilder.driverClassName("org.h2.Driver");
-	        dataSourceBuilder.url("jdbc:h2:tcp://localhost:9090/mem:sinsajodb");
-	        dataSourceBuilder.username("sa");
-	        dataSourceBuilder.password("");
-	        dataSource = dataSourceBuilder.build();
-		} catch (UnknownHostException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			DataSourceBuilder<DataSource> dataSourceBuilder = (DataSourceBuilder<DataSource>) DataSourceBuilder.create();
-	        dataSourceBuilder.driverClassName("org.h2.Driver");
-	        dataSourceBuilder.url("jdbc:h2:mem:productodb");
-	        dataSourceBuilder.username("sa");
-	        dataSourceBuilder.password("");
-			dataSource = dataSourceBuilder.build();
+			socket.close();
+			result = true;
+		} catch(IOException e) {
+			result = false;
 		}
 		
+		return result;
 	}
 
 }
