6aac721c0ba3c6b9f15a832845458df3c948bea6
vsanchcu <victoriano.sanchezcuenca@everis.nttdata.com>
2021-06-11 13:59:43 +0200

    Conf DataSource2

--- src/main/java/com/producto/configuration/AppConfiguration.java
+++ src/main/java/com/producto/configuration/AppConfiguration.java
@@ -6,13 +6,11 @@ import javax.sql.DataSource;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Primary;
 
 @Configuration
 public class AppConfiguration {
 	
-	@Primary
-	@Bean()
+	@Bean
 	public DataSource startConnection() {
 		
 		DataSourceBuilder<?> dataSourceBuilder = DataSourceBuilder.create();
